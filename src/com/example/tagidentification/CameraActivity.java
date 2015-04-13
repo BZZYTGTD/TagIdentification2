245e1f43034ca02e5f3fc02fb977391beacb9bac
BZZYTGTD <1322048858@qq.com>
2015-04-14 07:15:07 +0800

    不设置预览尺寸，直接存储，魅族三720*960

--- src/com/example/tagidentification/CameraActivity.java
+++ src/com/example/tagidentification/CameraActivity.java
@@ -5,12 +5,15 @@ import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.Array;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.http.util.EncodingUtils;
@@ -27,6 +30,7 @@ import org.opencv.imgproc.Imgproc;
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
@@ -52,6 +56,7 @@ import android.media.MediaScannerConnection.MediaScannerConnectionClient;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.storage.StorageManager;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Display;
@@ -271,7 +276,40 @@ public class CameraActivity extends Activity implements
 						
 					}
 				break;
-				default:
+			case TAKE_PICTURE:  
+                //将保存在本地的图片取出并缩小后显示在界面上  
+                 bitmap = BitmapFactory.decodeFile(
+                		 Environment.getExternalStorageDirectory()+"/image.jpg");  
+                 System.out.println("bitmap.getWidth() + "+bitmap.getWidth()+"bitmap.getHeight():"+bitmap.getHeight());
+ 	        	mCamera.stopPreview();  
+ 	        	 Matrix matrix = new Matrix();  
+ 	            matrix.postRotate((float)90.0);  
+ 	            rotaBitmap = Bitmap.createBitmap(bitmap, 
+ 	            		0, 0, bitmap.getWidth(), bitmap.getHeight(), 
+ 	            		matrix, false);
+ 	           System.out.println("rotaBitmap.getWidth() :"+ rotaBitmap.getWidth()+"rotaBitmap.getHeight()"+rotaBitmap.getHeight());
+ 	          int width,height;
+ 	          if(rotaBitmap.getWidth() > rotaBitmap.getHeight())    {
+ 	        	  width = rotaBitmap.getHeight();
+ 	        	  height = rotaBitmap.getHeight();
+ 	          }else{
+ 	        	  width = rotaBitmap.getWidth();
+ 	        	  height = rotaBitmap.getWidth();
+ 	          }
+ 	          sizeBitmap = Bitmap.createBitmap(rotaBitmap, 0,0,width, height);  
+ 	    	     //  System.out.println("sizeBitmap.getWidth()"+sizeBitmap.getWidth()+"sizeBitmap.getHeight()"+sizeBitmap.getHeight());
+ 	             if(null != sizeBitmap)  {
+ 	    	        		 savePhotos(sizeBitmap);
+ 	    	         }
+ 	       		
+ 	    		    // 重新预览
+ 	    	        mCamera.stopPreview();
+ 	    	        mCamera.startPreview();
+ 	    	        bitmap.recycle();//回收bitmap
+ 	    	        rotaBitmap.recycle();
+ 	    	        sizeBitmap.recycle();
+ 	    	       break;
+			default:
 					break;
 		}
 		
@@ -375,22 +413,45 @@ public class CameraActivity extends Activity implements
 		
 		public Bitmap sizeBitmap;
 		public Bitmap rotaBitmap;
+		
 	private PictureCallback mPicture = new PictureCallback() {
 
 	    @Override
 	    public void onPictureTaken(byte[] data, Camera camera) {
-
-	        if(null != data){  
-	        	bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);//data���ֽ���ݣ���������λͼ  
-	        	  System.out.println("bitmap.getWidth() + "+bitmap.getWidth()+"bitmap.getHeight():"+bitmap.getHeight());
-	        	mCamera.stopPreview();  
-//                isPreview = false;  
-            }  
-	        Matrix matrix = new Matrix();  
-            matrix.postRotate((float)90.0);  
-            rotaBitmap = Bitmap.createBitmap(bitmap, 
-            		0, 0, bitmap.getWidth(), bitmap.getHeight(), 
-            		matrix, false);
+	    	//480*640还是不行
+	    	File temFile = new File(Environment.getExternalStorageDirectory(),"image.jpg");  
+	    	
+	    	 try {
+	             FileOutputStream fos = new FileOutputStream(temFile);
+	             fos.write(data);
+	             fos.close();
+	         } catch (FileNotFoundException e) {
+	             Log.d(TAG, "File not found: " + e.getMessage());
+	         } catch (IOException e) {
+	             Log.d(TAG, "Error accessing file: " + e.getMessage());
+	         }
+//	    	bitmap = BitmapFactory.decodeFile(Environment.getExternalStorageDirectory()+"/image.jpg"); 
+//	    	 System.out.println("bitmap.getWidth() + "+bitmap.getWidth()+"bitmap.getHeight():"+bitmap.getHeight());
+//        	mCamera.stopPreview();
+	    	//也不行，还是缩略图的形式
+//	    	 if(null != data){
+//		    	 ByteArrayInputStream bais = new ByteArrayInputStream(data);
+//		    	 bitmap =  BitmapFactory.decodeStream(bais);
+//		    	 System.out.println("bitmap.getWidth() + "+bitmap.getWidth()+"bitmap.getHeight():"+bitmap.getHeight());
+//		        	mCamera.stopPreview();
+//	    	 }
+	    	//小米2手机是缩略图的形式640*480.这是不可以的。
+//	        if(null != data){  
+//	        	bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+//	        	  System.out.println("bitmap.getWidth() + "+bitmap.getWidth()+"bitmap.getHeight():"+bitmap.getHeight());
+//	        	mCamera.stopPreview();  
+////                isPreview = false;  
+//            }  
+//	        Matrix matrix = new Matrix();  
+//            matrix.postRotate((float)90.0);  
+//            rotaBitmap = Bitmap.createBitmap(bitmap, 
+//            		0, 0, bitmap.getWidth(), bitmap.getHeight(), 
+//            		matrix, false);
           
             //下面也可以实现截取图片的一部分为正方形区域
 //          Mat rotaBitmapMat = new Mat();
@@ -405,27 +466,27 @@ public class CameraActivity extends Activity implements
 //         Utils.matToBitmap(rotaBitmapMat, sizeBitmap);
 
           //     旋转之后3264*2448. 到2448 *3264
-      System.out.println("rotaBitmap.getWidth() :"+ rotaBitmap.getWidth()+"rotaBitmap.getHeight()"+rotaBitmap.getHeight());
-      int width,height;
-      if(rotaBitmap.getWidth() > rotaBitmap.getHeight())    {
-    	  width = rotaBitmap.getHeight();
-    	  height = rotaBitmap.getHeight();
-      }else{
-    	  width = rotaBitmap.getWidth();
-    	  height = rotaBitmap.getWidth();
-      }
-      sizeBitmap = Bitmap.createBitmap(rotaBitmap, 0,0,width, height);  
-	     //  System.out.println("sizeBitmap.getWidth()"+sizeBitmap.getWidth()+"sizeBitmap.getHeight()"+sizeBitmap.getHeight());
-         if(null != sizeBitmap)  {
-	        		 savePhotos(sizeBitmap);
-	         }
-   		
-		    // 重新预览
+//      System.out.println("rotaBitmap.getWidth() :"+ rotaBitmap.getWidth()+"rotaBitmap.getHeight()"+rotaBitmap.getHeight());
+//      int width,height;
+//      if(rotaBitmap.getWidth() > rotaBitmap.getHeight())    {
+//    	  width = rotaBitmap.getHeight();
+//    	  height = rotaBitmap.getHeight();
+//      }else{
+//    	  width = rotaBitmap.getWidth();
+//    	  height = rotaBitmap.getWidth();
+//      }
+//      sizeBitmap = Bitmap.createBitmap(rotaBitmap, 0,0,width, height);  
+//	     //  System.out.println("sizeBitmap.getWidth()"+sizeBitmap.getWidth()+"sizeBitmap.getHeight()"+sizeBitmap.getHeight());
+//         if(null != sizeBitmap)  {
+//	        		 savePhotos(sizeBitmap);
+//	         }
+//   		
+//		    // 重新预览
 	        mCamera.stopPreview();
 	        mCamera.startPreview();
-	        bitmap.recycle();//回收bitmap
-	        rotaBitmap.recycle();
-	        sizeBitmap.recycle();
+//	        bitmap.recycle();//回收bitmap
+//	        rotaBitmap.recycle();
+//	        sizeBitmap.recycle();
 	    }
 
 	};
@@ -438,15 +499,17 @@ public class CameraActivity extends Activity implements
 	public void savePhotos(Bitmap bm){  
 
 	    File folder = new File(Environment.getExternalStoragePublicDirectory(
-	              Environment.DIRECTORY_PICTURES), "MyTagApp");
+	              Environment.DIRECTORY_DCIM), "Camera");
+		
 //         savePath = "/mnt/sdcard/MyTagApp/";  
 //        File folder = new File(savePath);  
         if(!folder.exists()) 
         {  
             folder.mkdir();  
         }  
-        long dataTake = System.currentTimeMillis();  
-         jpegName = folder.getPath() + File.separator + dataTake +".jpg";  
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+//        long dataTake = System.currentTimeMillis();  
+         jpegName = folder.getPath() + File.separator + timeStamp +".jpg";  
         Log.i(TAG, "saveJpeg:jpegName--" + jpegName);  
         try {  
         	System.out.println("jpegName:  "+jpegName);
@@ -465,24 +528,27 @@ public class CameraActivity extends Activity implements
             Log.i(TAG, "Photos save  failed!");  
             e.printStackTrace();  
         }  
-        try {
-			ExifInterface exif = new ExifInterface("/mnt/sdcard/DCIM/123.jpg");
-//			exif.setAttribute(ExifInterface.TAG_APERTURE,FNumber);
-//			exif.setAttribute(ExifInterface.TAG_EXPOSURE_TIME,Exposure_Time);
-			
-			FNumber = exif.getAttribute(ExifInterface.TAG_APERTURE);
-			Exposure_Time = exif.getAttribute(ExifInterface.TAG_EXPOSURE_TIME);
-			System.out.println("FNumber : "+FNumber+"Exposure_Time :" + Exposure_Time);
-			
-        } catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        Uri localUri = Uri.fromFile(folder);
-
-        Intent localIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, localUri);
-
-        sendBroadcast(localIntent);
+//        try {
+//			ExifInterface exif = new ExifInterface("/mnt/sdcard/DCIM/123.jpg");
+////			exif.setAttribute(ExifInterface.TAG_APERTURE,FNumber);
+////			exif.setAttribute(ExifInterface.TAG_EXPOSURE_TIME,Exposure_Time);
+//			
+//			FNumber = exif.getAttribute(ExifInterface.TAG_APERTURE);
+//			Exposure_Time = exif.getAttribute(ExifInterface.TAG_EXPOSURE_TIME);
+//			System.out.println("FNumber : "+FNumber+"Exposure_Time :" + Exposure_Time);
+//			
+//        } catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+        //以下两种都不能及时刷新图库
+//        Uri localUri = Uri.fromFile(folder);
+//
+//        Intent localIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, localUri);
+//
+//        sendBroadcast(localIntent);
+        MediaScannerConnection.scanFile(this, new String[]{Environment.getExternalStoragePublicDirectory(
+        		Environment.DIRECTORY_DCIM).getPath() + "/" + "Camera"}, null, null);  
     }  
 	private String readPath;
 	public Bitmap readPhotos(Bitmap bm){
@@ -634,6 +700,7 @@ public class CameraActivity extends Activity implements
              case R.id.button_capture: //����
                      mCamera.autoFocus(this);//自动聚焦
                      mCamera.takePicture(null, null, mPicture);
+                     
                      Toast.makeText(getApplicationContext(), "Yes", Toast.LENGTH_SHORT).show();
                      
                      break;
@@ -794,6 +861,10 @@ public class CameraActivity extends Activity implements
 		}
 //		
 		private Camera.Size cs;
+		private float psizeheight;
+		private float psizewidth;
+		private float sizeheight;
+		private float sizewidth;
 		@Override
 		public void surfaceChanged(SurfaceHolder holder, int format, int width,
 				int height) {
@@ -834,13 +905,28 @@ public class CameraActivity extends Activity implements
 			
 			
 			params.setPictureFormat(PixelFormat.JPEG);  
- 	          params.setPreviewSize(800, 600); 
+
+//	          List<Camera.Size> psizes = params.getSupportedPictureSizes(); 
+//	            Camera.Size pcs = (Camera.Size) psizes.get(0); 
+//	            psizeheight = pcs.height; 
+// 	            psizewidth = pcs.width;
+//// 	            params.setPictureSize((int)psizewidth, (int)psizeheight);
+// 	            float n = psizeheight/psizewidth;
 // 	          List<Camera.Size> sizes = params.getSupportedPreviewSizes(); 
+// 	         for (int i = 0; i < sizes.size(); i++) { 
+// 	            Camera.Size cs = (Camera.Size) sizes.get(i); 
+// 	             sizeheight = cs.height; 
+// 	             sizewidth = cs.width; 
+// 	             if(n == (sizeheight/ sizewidth)){
+// 	            	params.setPreviewSize((int)sizewidth, (int)sizeheight);
+// 	            	break;
+// 	             }
+// 	         }
 // 	          cs = sizes.get(1); 
 // 	        params.setPreviewSize(cs.width, cs.height);//800*600
-// 	           System.out.println("cs.width"+ cs.width+ "cs.height"+ cs.height);
- 	           
-			 params.setJpegQuality(100);// ������Ƭ����
+// 	           System.out.println("cs.width"+ cs.width + "cs.height"+ cs.height);
+
+			params.setJpegQuality(100);// ������Ƭ����
 			 params.setRotation(90);  
 			 mCamera.setParameters(params);
 			 mCamera.setDisplayOrientation(90);
@@ -873,8 +959,23 @@ public class CameraActivity extends Activity implements
                  mCamera = Camera.open(); 
        	         params = mCamera.getParameters();
        	         params.setPictureFormat(PixelFormat.JPEG);  
-				 
-       	         params.setPreviewSize(800, 600);   
+//				 params.setPreviewSize(640, 480);
+//       	      List<Camera.Size> psizes = params.getSupportedPictureSizes(); 
+//	            Camera.Size pcs = (Camera.Size) psizes.get(0); 
+//	            psizeheight = pcs.height; 
+//	            psizewidth = pcs.width;
+////	            params.setPictureSize((int)psizewidth, (int)psizeheight);
+//	            float n = psizeheight/psizewidth;
+//	          List<Camera.Size> sizes = params.getSupportedPreviewSizes(); 
+//	         for (int i = 0; i < sizes.size(); i++) { 
+//	            Camera.Size cs = (Camera.Size) sizes.get(i); 
+//	             sizeheight = cs.height; 
+//	             sizewidth = cs.width; 
+//	             if(n == (sizeheight/ sizewidth)){
+//	            	params.setPreviewSize((int)sizewidth, (int)sizeheight);
+//	            	break;
+//	             }
+//	         }
 				 params.setJpegQuality(100);// ������Ƭ����
 				 params.setRotation(90);  
 				 mCamera.setParameters(params);
