c1bf62eddb810d33210f2e930b089d2c086921f8
BZZYTGTD <1322048858@qq.com>
2015-04-21 12:38:03 +0800

    添加了使用功能简介，但是从欢迎界面直接到程序的话，会使得，超出图片内存

--- src/com/example/tagidentification/MainActivity.java
+++ src/com/example/tagidentification/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.RelativeLayout;
-
+//MainActivity暂时没用到
 public class MainActivity extends Activity {
 	 RelativeLayout soso;
     @Override
